63391c164a4d648a1863790faf86e1593a80e25d
Ary Okoli <aco.umeadi@gmail.com>
2023-04-17 15:35:06 -0400

    tasklister mini project lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+document.addEventListener("DOMContentLoaded", (event) => {
+  const form = document.getElementById("create-task-form")
+  form.addEventListener("click", createTaskToDo)
+})
+
+function createTaskToDo(event) {
+  event.preventDefault()
+  const task = document.querySelector("#new-task-description").value
+  let newDiv = document.createElement("div")
+  document.getElementById("tasks").appendChild(newDiv)
+  newDiv.textContent = task
+  }
+  
+
